d60d53005cd1582ded345b36009b3476d65d21fe
ZARIFMAC <uzbzarif@gmail.com>
2022-11-11 10:07:37 +0500

    get fruits name

---
+++ get_fruits_name.py
@@ -0,0 +1,10 @@
+def get_frutis_name(data:str)->list:
+    """
+    This function returns the name of the fruits in the list
+
+    args:
+        data: CSV file with the fruits data
+    returns:
+        list of fruits names
+    """
+    
