71fc82e5c97275890731a3f9cbf131d634c4343f
turonov <turonov5400gmail.com>
2022-11-14 09:19:51 +0500

    birinchi

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -8,4 +8,25 @@ def get_cheapest_fruit(data:str)->str:
         name of the cheapest fruit
     """
     # your code here
-    pass
+   
+    def get_cheapest_fruit(data: str) -> str:
+        d = []
+        a = []
+        ans = 0
+        ans1 = ''
+    row = data.split("\n")[1:]
+    for i in row:
+        d.append(float(i.split(',')[1]))
+        a.append(str(i.split(",")[0]))
+
+    ans = d[0]
+    ans1 = a[0]
+    for i in range(len(d)-1):
+        if d[i] < ans:
+            ans = d[i]
+            ans1 = a[i]
+
+    return ans1
+
+data = open("fruits.csv").read()
+print(get_cheapest_fruit(data))
